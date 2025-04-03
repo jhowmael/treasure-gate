1c8d942f0d31e1583b38c63c1ea8559e03f4e465
Rhels <rhels.ziliani@gmail.com>
2025-04-03 19:34:08 -0300

    Wallet-Signatures change front

--- resources/views/signatures.blade.php
+++ resources/views/signatures.blade.php
@@ -1,35 +1,101 @@
 @extends('layouts.web') <!-- Referência ao seu layout 'web' -->
 
 @section('content')
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6 mb-4">
-            <div class="card">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Plano de 1 Mês</h5>
-                    <p class="card-text">Plano mensal para quem precisa de flexibilidade.</p>
-                    <h3>R$ {{ config('payments.values.monthly') }}</h3>
-                    <form action="{{ route('monthly-checkout') }}" method="POST">
-                        @csrf
-                        <button type="submit" class="btn btn-success">Assine Agora</button>
-                    </form>
-                </div>
-            </div>
-        </div>
 
-        <div class="col-md-6 mb-4">
-            <div class="card">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Plano de 1 Ano</h5>
-                    <p class="card-text">Aproveite o melhor preço para o plano de 1 ano.</p>
-                    <h3>R$ {{ config('payments.values.yearly') }}</h3>
-                    <form action="{{ route('yearly-checkout') }}" method="POST">
+<!-- From Uiverse.io by Yaya12085 --> 
+ <div  class="flex items-stretch md:items-center ...">
+<div class="plan mr-5">
+		<div class="inner">
+			<span class="pricing">
+				<span>
+                R$ {{ config('payments.values.monthly') }}<small>/mês</small>
+				</span>
+			</span>
+			<p class="title">Plano de 1 Mês</p>
+			<p class="info">Plano mensal para quem precisa de flexibilidade.</p>
+			<ul class="features">
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span><strong>24h </strong>Atendimentos</span>
+				</li>
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span>Área <strong>de membros</strong></span>
+				</li>
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span>Grupo ao vivo de operação no discord</span>
+				</li>
+			</ul>
+			<div class="action">
+            <form action="{{ route('monthly-checkout') }}" method="POST">
                         @csrf
-                        <button type="submit" class="btn btn-success">Assine Agora</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+                        <button type="submit" class="button btn btn-success">Assine Agora</button>
+            </form>
+			</div>
+		</div>
+	</div>
+    <div class="plan">
+    <div class="inner">
+			<span class="pricing">
+				<span>
+                R$ {{ config('payments.values.yearly') }}<small>/ano</small>
+				</span>
+			</span>
+			<p class="title">Plano de 1 Ano</p>
+			<p class="info">Aproveite o melhor preço para o plano de 1 ano.</p>
+			<ul class="features">
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span><strong>24h </strong>Atendimentos</span>
+				</li>
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span>Área <strong>de membros</strong></span>
+				</li>
+				<li>
+					<span class="icon">
+						<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+						</svg>
+					</span>
+					<span>Grupo ao vivo de operação no discord</span>
+				</li>
+			</ul>
+			<div class="action">
+            <form action="{{ route('yearly-checkout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="button btn btn-success">Assine Agora</button>
+            </form>
+			</div>
+		</div>
     </div>
+
 </div>
 @endsection
--- resources/views/wallet.blade.php
+++ resources/views/wallet.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.web')
 
 @section('content')
+<div class="container mx-auto px-4 py-8 max-w-md">
+    <div class="grid grid-cols-2 gap-3">
+      
+       
+    <div class="arbitrage-card">
+        <h2 class="text-xl text-white font-semibold mb-2"><img class="w-7 h-7" src="{{ asset('mexc-logo.svg') }}" alt="MEXC Logo"><strong>MECX    $100</h2>
+        <a href="javascript:void(0);"class="button-link mt-auto bg-purple-500 text-white py-2 px-4 rounded-lg">Alterar Saldo</a>
+      </div>
+      
+      <div class="arbitrage-card">
+        <h2 class="text-xl text-white font-semibold mb-2"><img class="w-7 h-7" src="{{ asset('gate.io.svg') }}"alt="Gate Logo"><strong>Gate    $100</h2>
+        <a href="javascript:void(0);"class="button-link mt-auto bg-purple-500 text-white py-2 px-4 rounded-lg">Alterar Saldo</a>
+      </div>
 
+    </div>
+  </div>
 @endsection
